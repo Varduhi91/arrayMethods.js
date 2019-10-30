0edf2e22acae96b5909392a9f7d9ea933c281b64
Varduhi91 <57092808+Varduhi91@users.noreply.github.com>
2019-10-30 20:46:40 +0400

    Create map.js

---
+++ map.js
@@ -0,0 +1,7 @@
+let arr = ["Barcelona", "Real", "Liverpool", "Arsenal"];
+let arr1 = [];
+
+  arr.forEach(function(item){
+      arr1.push(item.length) })
+
+ document.write("The array items lengths are: " + arr1);
